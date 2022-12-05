c6c906266a5035771f81b1ba9f26ad308f39193d
Henrik Fricke <henrikfricke@web.de>
2022-12-05 23:05:18 +0100

    refa: use netlify edge functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
 export default defineConfig({
