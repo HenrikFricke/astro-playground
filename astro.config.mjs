0f058d2a688233eec8d7f342dd63684716187da2
Henrik Fricke <henrikfricke@web.de>
2022-12-06 15:31:15 +0100

    refa: go back to cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: cloudflare(),
   integrations: [tailwind()],
   vite: {
     define: {
